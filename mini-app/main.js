704637048de9ce580153997b061941415de3f5eb
Grazi GS <graziwho@gmail.com>
2018-06-23 16:34:05 -0300

    Lord, help me

--- mini-app/main.js
+++ mini-app/main.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello! \n");
+  response.end("Hello getintodevops.com\n");
 });
 
 server.listen(8000);
