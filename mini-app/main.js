7e87146577c8106d546683819603687b8e5ba969
Grazi GS <graziwho@gmail.com>
2018-06-23 16:52:24 -0300

    Fix ufa!

--- mini-app/main.js
+++ mini-app/main.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello getintodevops.com\n");
+  response.end("Hello SkipTheDishes! <3\n");
 });
 
 server.listen(8000);
